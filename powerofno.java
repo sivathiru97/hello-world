997479a4d37ee689a503058acb9b703d6494405b
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-06-07 12:24:50 +0530

    Update powerofno.java

--- powerofno.java
+++ powerofno.java
@@ -3,25 +3,39 @@ import java.util.*;
 public class powerofno
 {
 public static void main(String args[])
-{
- int n=5,p=3,result=1;
- 
-    if(n>=0&&p==0)
-     {
-        result=1;
-     }
-    else if(n==0&&p>=1)
-      { 
-         result=0;
-      }
-    else
-     {
-         for(int i=1;i<=p;i++)
-	 {
-            result=result*n;
- 	 }	    
-     }
-    System.out.println(n+"^"+p+"="+result);
 	
-  }
+   {
+	
+       long n,p,r=1;
+	
+       Scanner sc=new Scanner(System.in);
+ 
+       System.out.println("enter number");
+	  
+       n=sc.nextLong();
+ 
+       System.out.println("enter power");
+	  
+       p=sc.nextLong();
+ 
+       if(n>=0&&p==0)
+       {   
+          r =1;
+       }
+       else if(n==0&&p>=1)
+	{   
+            r=0;
+	} 
+       else
+	{ 
+              for(int i=1;i<=p;i++)
+	       { 
+                   r=r *n;
+ 	 	}    
+  	 }    
+ 
+    	  System.out.println(n+"^"+p+"="+r);
+ 
+    }
+ 
 }
