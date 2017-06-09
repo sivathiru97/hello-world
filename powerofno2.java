ff70ce0b2af950e1bf425804fb46fe01ed9ff825
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-06-09 18:15:17 +0530

    Create powerofno2.java

---
+++ powerofno2.java
@@ -0,0 +1,13 @@
+import java.io.*
+import java.util.*;
+import java.lang;
+class powerofno
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m=sc.nextInt();
+System.out.print(Math.power(n,m));
+}
+}
