89481e3049c223a78e2deed019e42839baae18dd
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-06-12 11:24:19 +0530

    Create FibonacciEx1.java

---
+++ FibonacciEx1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class FibonacciEx1
+{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);
+    
+ for(i=2;i<count;++i)
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+}}  
