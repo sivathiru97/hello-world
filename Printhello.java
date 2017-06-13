e0215b578e66e0c950adb5f70007b32256d77eef
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-06-13 10:32:38 +0530

    Create Printhello.java

---
+++ Printhello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Printhello
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(i=0;i<n;i++)
+{
+System.out.println("Hello");
+}
+}
+}
