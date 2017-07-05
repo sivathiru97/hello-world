d7a372502f5802e6d9acfabe18486cd99b012708
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-07-05 09:44:22 +0530

    Update count.java

--- count.java
+++ count.java
@@ -1,17 +1,20 @@
 import java.io.*;
 import java.util.*;
-class count
+public class count
 {
-public static void main(String args[])
-{
-int n=0;
-Scanner sc=new Scanner(System.in);
-String s=sc.nextInt();
-char c[]=s.tocharArray();
-for(i=0;i<s.length;i++)
-{
-n++;
+    public static void main(String args[])
+    {
+        int count=0;
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char c[]=s.toCharArray();
+        for(int i=0;i<s.length();i++)
+        {
+        count++;
+        }
+        System.out.println(count);
+    }
 }
-System.out.pritnln(n);
-}
-}
+
+
+
