474d52a181c3fa532f6491942138a4a76d80a43c
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-07-06 09:40:53 +0530

    Create stringcon.java

---
+++ stringcon.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class stringcon
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        String str1=sc.nextLine();
+        String str2=sc.nextLine();
+           System.out.print(str1 + str2);
+        
+        
+    }
+}
