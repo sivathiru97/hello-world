7a4fa0a5e12e10a7a29f9613b3aa703375837e24
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-07-06 09:35:07 +0530

    Create stringcmp.java

---
+++ stringcmp.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class guviset5q42
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        String str1=sc.nextLine();
+        String str2=sc.nextLine();
+        if(str1.equals(str2))
+        {
+            System.out.print("given two strings are equal");
+        }
+        else
+        {
+            System.out.print("given two strings are not equal");
+        }
+        
+    }
+}
