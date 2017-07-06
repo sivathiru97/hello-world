f06ab122344c4963d1a25e866b721026b27a7135
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-07-06 09:26:19 +0530

    Create asciivalue.java

---
+++ asciivalue.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class guviascii
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<=127;i++)
+        {
+            System.out.print((char)(0+i));
+        }
+}
+}
+
+
+
