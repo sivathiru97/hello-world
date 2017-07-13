780e1fa043fa5e3b7dc4d84c05a086417444515b
sivathiru97 <sivaranjanithiru1997@gmail.com>
2017-07-13 11:17:02 +0530

    Create revstring.java

---
+++ revstring.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class revstring {
+ 
+public static void main(String[] args) {
+ 
+String str="Hello world";
+String revstring="";
+for(int i=str.length()-1;i>=0;--i){
+revstring +=str.charAt(i);
+}
+ 
+System.out.println(revstring);
+}
+}
